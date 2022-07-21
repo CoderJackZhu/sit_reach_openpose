df7b10e17dc098ba34cbecb7951bf6c6abe634f6
Jack <42195616+CoderJackZhu@users.noreply.github.com>
2022-07-21 14:01:37 +0800

    1.2 publish finish frame decide

--- fix_frame.py
+++ fix_frame.py
@@ -1,11 +1,10 @@
 import copy
-import csv
 import os
 
 import cv2
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
+
 
 from src import util
 from src.body import Body
@@ -154,10 +153,9 @@ def cal_one_frame(test_image, keen_y=470):
 def cal_multi_vid(root_dir='result'):
     ori_data = pd.read_csv('./sit_reach_data.csv', header=0)
     files = os.listdir(root_dir)
-    files.sor()
-    row_list = [
-        ['label_frame', 'meas_frame', 'label_len', 'meas_len']
-    ]
+    files.sort()
+    row_list = ['label_frame', 'meas_frame', 'label_len', 'meas_len']
+    result_file = pd.DataFrame(columns=row_list)
     for file in files:
         id = int(file[-3:])
         frame = ori_data.iloc[id - 1, 3]
@@ -183,15 +181,16 @@ def cal_multi_vid(root_dir='result'):
             result = (large_hand_x - ins[mode, 0, 0]) / (ins[mode, 1, 0] - ins[mode, 0, 0]) * 80
             result = result.round(2)
             print('记录成绩为{}'.format(ori_data.iloc[id - 1, 4]))
-            print(f'成绩为{result}')
+            print(f'预测成绩为{result}')
             print(f'最远帧为{large_site}')
         else:
             result = 0
             print('最远手的位置不在范围内')
-        row_list.append([frame, large_site, ori_data.iloc[id - 1, 4], result])
-    with open('./result.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerows(row_list)
+        result_file.loc[id] = [frame, large_site, ori_data.iloc[id - 1, 4], result]
+        result_file.to_csv('./result_file.csv', index=False)
+    # with open('./result.csv', 'w', newline='') as f:
+    #     writer = csv.writer(f)
+    #     writer.writerows(row_list)
 
 
 if __name__ == '__main__':
