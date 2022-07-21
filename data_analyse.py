6027456692170502fe62446052c7f7483c7a339d
Jack <42195616+CoderJackZhu@users.noreply.github.com>
2022-07-21 17:31:57 +0800

    1.3 publish finish frame decide

--- data_analyse.py
+++ data_analyse.py
@@ -3,19 +3,22 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-file = pd.read_csv('./坐位体前屈算法结果.csv', header=None)
-data = file.iloc[:, 0:3]
+file = pd.read_csv('./result.csv', header=0)
+# data = file.iloc[:, 0:3]
+data = file.iloc[:, 0:4]
 none_len = np.sum(data.iloc[:, 1] == -1)
 print(f'未找到手的个数为{none_len}')
 absolute_error_list = []
 relative_error_list = []
 for i in range(data.shape[0]):
-    if data.iloc[i, 1] != -1:
-        absolute_error = data.iloc[i, 1] - data.iloc[i, 0]
-        relative_error = absolute_error / data.iloc[i, 0]
+    if data.iloc[i, 3] != -1:
+        absolute_error = data.iloc[i, 3] - data.iloc[i, 2]
+        relative_error = absolute_error / data.iloc[i, 2]
         absolute_error_list.append(absolute_error)
         relative_error_list.append(relative_error)
-# data.iloc[:,1].hist()
+data.iloc[:, 3].hist()
+plt.title('data histogram')
+plt.show()
 average_error = sum(map(abs, absolute_error_list)) / len(absolute_error_list)
 print('平均误差为{:.2f}'.format(average_error))
 plt.hist(absolute_error_list)
